beedeea426abe0c004e0efe64e6f37d6c259f7a2
Rene Argento <rene.argento@gmail.com>
2020-08-28 01:55:14 +0200

    Added exercise 1.3.4.1.1

---
+++ src/chapter1/section3/section4/exercise1/Exercise1.java
@@ -0,0 +1,16 @@
+package chapter1.section3.section4.exercise1;
+
+import java.util.Scanner;
+
+/**
+ * Created by Rene Argento on 28/08/20.
+ */
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number = scanner.nextDouble();
+        System.out.printf("%7.3f\n", number);
+    }
+
+}
