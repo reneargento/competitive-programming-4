165cb43bd7ff817d13fe4e60a09ac71c9f070bbc
Rene Argento <rene.argento@gmail.com>
2020-09-17 01:03:55 +0200

    Added problem Hello

---
+++ src/chapter1/section4/a/io/sequences/Hello.java
@@ -0,0 +1,12 @@
+package chapter1.section4.a.io.sequences;
+
+/**
+ * Created by Rene Argento on 04/09/20.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
