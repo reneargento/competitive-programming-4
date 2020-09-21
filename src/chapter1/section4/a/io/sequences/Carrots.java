54f4929917c166ad5aad5ab8e354135d94725813
Rene Argento <rene.argento@gmail.com>
2020-09-22 01:16:43 +0200

    Added problem Carrots

---
+++ src/chapter1/section4/a/io/sequences/Carrots.java
@@ -0,0 +1,16 @@
+package chapter1.section4.a.io.sequences;
+
+import java.util.Scanner;
+
+/**
+ * Created by Rene Argento on 04/09/20.
+ */
+public class Carrots {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+        System.out.println(scanner.nextInt());
+    }
+
+}
