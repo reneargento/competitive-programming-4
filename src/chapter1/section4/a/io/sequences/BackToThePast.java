35ba548213d38262397bafd0c861e7c6448b8786
Rene Argento <rene.argento@gmail.com>
2020-09-21 20:39:42 +0200

    Added problem BackToThePast

---
+++ src/chapter1/section4/a/io/sequences/BackToThePast.java
@@ -0,0 +1,12 @@
+package chapter1.section4.a.io.sequences;
+
+/**
+ * Created by Rene Argento on 04/09/20.
+ */
+public class BackToThePast {
+
+    public static void main(String[] args) {
+        System.out.println("May 29, 2013 Wednesday");
+    }
+
+}
