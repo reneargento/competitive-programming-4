4fd2cff559554aeb6147c650752709119175ff8a
Rene Argento <rene.argento@gmail.com>
2020-09-24 02:00:50 +0200

    Added problem TheLastProblem

---
+++ src/chapter1/section4/a/io/sequences/TheLastProblem.java
@@ -0,0 +1,16 @@
+package chapter1.section4.a.io.sequences;
+
+import java.util.Scanner;
+
+/**
+ * Created by Rene Argento on 04/09/20.
+ */
+public class TheLastProblem {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Thank you, " + name + ", and farewell!");
+    }
+
+}
