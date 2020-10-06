a6841e6d914414d3d5a604162d229692f3712fd0
Rene Argento <rene.argento@gmail.com>
2020-10-06 02:02:10 +0200

    Added problem CelebrityJeopardy

---
+++ src/chapter1/section4/b/repetition/CelebrityJeopardy.java
@@ -0,0 +1,18 @@
+package chapter1.section4.b.repetition;
+
+import java.util.Scanner;
+
+/**
+ * Created by Rene Argento on 04/09/20.
+ */
+public class CelebrityJeopardy {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
+    }
+
+}
