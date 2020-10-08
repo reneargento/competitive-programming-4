c8e256fff9f6c9bb975a8deb9135703af77ecb7c
Rene Argento <rene.argento@gmail.com>
2020-10-09 01:38:42 +0200

    Added problem Different

---
+++ src/chapter1/section4/b/repetition/Different.java
@@ -0,0 +1,17 @@
+package chapter1.section4.b.repetition;
+
+import java.util.Scanner;
+
+/**
+ * Created by Rene Argento on 04/09/20.
+ */
+public class Different {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            System.out.println(Math.abs(scanner.nextLong() - scanner.nextLong()));
+        }
+    }
+
+}
