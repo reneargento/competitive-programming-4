f2e7907525536bea409ace190353cac676648b36
Rene Argento <rene.argento@gmail.com>
2022-01-12 20:14:08 -0300

    Added some 3.2.i section algorithms

--- src/chapter3/section2/i/mathematical/simulation/harder/PrimaryArithmetic.java
+++ src/chapter3/section2/i/mathematical/simulation/harder/PrimaryArithmetic.java
@@ -16,7 +16,14 @@ public class PrimaryArithmetic {
 
         while (number1 != 0 || number2 != 0) {
             int carryOperations = countCarryOperations(number1, number2);
-            
+
+            if (carryOperations == 0) {
+                outputWriter.printLine("No carry operation.");
+            } else if (carryOperations == 1) {
+                outputWriter.printLine("1 carry operation.");
+            } else {
+                outputWriter.printLine(String.format("%d carry operations.", carryOperations));
+            }
 
             number1 = FastReader.nextInt();
             number2 = FastReader.nextInt();
@@ -25,8 +32,37 @@ public class PrimaryArithmetic {
     }
 
     private static int countCarryOperations(int number1, int number2) {
+        int carryOperations = 0;
+        int length1 = String.valueOf(number1).length();
+        int length2 = String.valueOf(number2).length();
+        int arrayLength = Math.max(length1, length2);
 
+        int[] digits1 = getDigits(number1, arrayLength);
+        int[] digits2 = getDigits(number2, arrayLength);
 
+        for (int i = arrayLength - 1; i >= 0; i--) {
+            int sum = digits1[i] + digits2[i];
+            if (sum >= 10) {
+                carryOperations++;
+                if (i > 0) {
+                    digits1[i - 1]++;
+                }
+            }
+        }
+        return carryOperations;
+    }
+
+    private static int[] getDigits(int number, int arrayLength) {
+        int[] digits = new int[arrayLength];
+        int index = arrayLength - 1;
+
+        while (number > 0) {
+            int digit = number % 10;
+            digits[index] = digit;
+            index--;
+            number /= 10;
+        }
+        return digits;
     }
 
     private static class FastReader {
