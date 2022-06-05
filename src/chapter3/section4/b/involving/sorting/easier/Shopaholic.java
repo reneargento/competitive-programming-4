6626ccd86cbf12c4094bd408735e8dd86ac6e8b8
Rene Argento <rene.argento@gmail.com>
2022-06-05 02:10:18 +0200

    Added some 3.4.b section algorithms

---
+++ src/chapter3/section4/b/involving/sorting/easier/Shopaholic.java
@@ -0,0 +1,89 @@
+package chapter3.section4.b.involving.sorting.easier;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 27/05/22.
+ */
+public class Shopaholic {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        int tests = FastReader.nextInt();
+
+        for (int t = 0; t < tests; t++) {
+            int[] prices = new int[FastReader.nextInt()];
+            for (int i = 0; i < prices.length; i++) {
+                prices[i] = FastReader.nextInt();
+            }
+
+            long maximumDiscount = computeMaximumDiscount(prices);
+            outputWriter.printLine(maximumDiscount);
+        }
+        outputWriter.flush();
+    }
+
+    private static long computeMaximumDiscount(int[] prices) {
+        long maximumDiscount = 0;
+        Arrays.sort(prices);
+
+        for (int i = prices.length - 3; i >= 0; i -= 3) {
+            maximumDiscount += prices[i];
+        }
+        return maximumDiscount;
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
