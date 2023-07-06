76c6413b8556637fa9ce9cb7df87e9871993b2c6
Rene Argento <rene.argento@gmail.com>
2023-07-06 23:03:03 +0200

    Added some 4.3.a section algorithms

---
+++ src/chapter4/section3/a/standard/HeavyCycleEdges.java
@@ -0,0 +1,180 @@
+package chapter4.section3.a.standard;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 03/07/23.
+ */
+public class HeavyCycleEdges {
+
+    private static class Edge implements Comparable<Edge> {
+        int vertex1;
+        int vertex2;
+        int cost;
+
+        Edge(int vertex1, int vertex2, int cost) {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge other) {
+            return Integer.compare(cost, other.cost);
+        }
+    }
+
+    private static class KruskalMinimumSpanningTree {
+
+        private static class UnionFind {
+            private final int[] leaders;
+            private final int[] ranks;
+            private int components;
+
+            public UnionFind(int size) {
+                leaders = new int[size];
+                ranks = new int[size];
+                components = size;
+
+                for (int i = 0; i < size; i++) {
+                    leaders[i] = i;
+                    ranks[i] = 0;
+                }
+            }
+
+            public int count() {
+                return components;
+            }
+
+            public boolean connected(int site1, int site2) {
+                return find(site1) == find(site2);
+            }
+
+            public int find(int site) {
+                if (site == leaders[site]) {
+                    return site;
+                }
+                return leaders[site] = find(leaders[site]);
+            }
+
+            public void union(int site1, int site2) {
+                int leader1 = find(site1);
+                int leader2 = find(site2);
+                if (leader1 == leader2) {
+                    return;
+                }
+
+                if (ranks[leader1] < ranks[leader2]) {
+                    leaders[leader1] = leader2;
+                } else if (ranks[leader2] < ranks[leader1]) {
+                    leaders[leader2] = leader1;
+                } else {
+                    leaders[leader1] = leader2;
+                    ranks[leader2]++;
+                }
+                components--;
+            }
+        }
+
+        public static List<Integer> getHeavyCycleWeights(List<Edge> edges, int totalVertices) {
+            List<Integer> heavyCycleWeights = new ArrayList<>();
+            Collections.sort(edges);
+            KruskalMinimumSpanningTree.UnionFind unionFind = new KruskalMinimumSpanningTree.UnionFind(totalVertices + 1);
+
+            for (Edge edge : edges) {
+                if (!unionFind.connected(edge.vertex1, edge.vertex2)) {
+                    unionFind.union(edge.vertex1, edge.vertex2);
+                } else {
+                    heavyCycleWeights.add(edge.cost);
+                }
+
+                if (unionFind.components == 1) {
+                    break;
+                }
+            }
+            Collections.sort(heavyCycleWeights);
+            return heavyCycleWeights;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        int nodes = FastReader.nextInt();
+        int edges = FastReader.nextInt();
+
+        while (nodes != 0 || edges != 0) {
+            List<Edge> edgesList = new ArrayList<>();
+            for (int i = 0; i < edges; i++) {
+                Edge edge = new Edge(FastReader.nextInt(), FastReader.nextInt(), FastReader.nextInt());
+                edgesList.add(edge);
+            }
+
+            List<Integer> heavyCycleWeights = KruskalMinimumSpanningTree.getHeavyCycleWeights(edgesList, nodes);
+            if (heavyCycleWeights.isEmpty()) {
+                outputWriter.printLine("forest");
+            } else {
+                outputWriter.print(heavyCycleWeights.get(0));
+                for (int i = 1; i < heavyCycleWeights.size(); i++) {
+                    outputWriter.print(" " + heavyCycleWeights.get(i));
+                }
+                outputWriter.printLine();
+            }
+
+            nodes = FastReader.nextInt();
+            edges = FastReader.nextInt();
+        }
+        outputWriter.flush();
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
