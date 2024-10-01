00064058628d18eff24c6184922910d09550ef6c
Rene Argento <rene.argento@gmail.com>
2024-10-01 03:12:07 +0200

    Added 5.2.a section algorithms

---
+++ src/chapter5/section2/a/finding/simple/formula/or/pattern/easier/BubbleSort.java
@@ -0,0 +1,80 @@
+package chapter5.section2.a.finding.simple.formula.or.pattern.easier;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 19/09/24.
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        int tests = FastReader.nextInt();
+
+        for (int t = 1; t <= tests; t++) {
+            int arraySize = FastReader.nextInt();
+            String averageCounts = computeAverageCounts(arraySize);
+            outputWriter.printLine(String.format("Case %d: %s", t, averageCounts));
+        }
+        outputWriter.flush();
+    }
+
+    private static String computeAverageCounts(long arraySize) {
+        long averageSwaps = (arraySize * (arraySize - 1)) / 2;
+
+        if (averageSwaps % 2 == 0) {
+            return String.valueOf(averageSwaps /  2);
+        } else {
+            return averageSwaps + "/2";
+        }
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
