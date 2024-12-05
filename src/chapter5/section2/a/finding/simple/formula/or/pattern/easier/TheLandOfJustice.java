3815d2cdb67ed7b8dfd304f9ace80a15e62be8f5
Rene Argento <rene.argento@gmail.com>
2024-12-04 23:33:28 -0300

    Added 5.2.a section algorithms

---
+++ src/chapter5/section2/a/finding/simple/formula/or/pattern/easier/TheLandOfJustice.java
@@ -0,0 +1,73 @@
+package chapter5.section2.a.finding.simple.formula.or.pattern.easier;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 01/12/24.
+ */
+public class TheLandOfJustice {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        int sphereParts = FastReader.nextInt();
+
+        while (sphereParts > 0) {
+            long profit = 0;
+            if (sphereParts > 1) {
+                profit = sphereParts * 25L;
+            }
+            outputWriter.printLine(profit + "%");
+            sphereParts = FastReader.nextInt();
+        }
+        outputWriter.flush();
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
