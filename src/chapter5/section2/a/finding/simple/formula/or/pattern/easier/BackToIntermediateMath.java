39819c47a4a2193a6a91759d1c9d7d8901c4d6f0
Rene Argento <rene.argento@gmail.com>
2024-12-09 21:04:33 -0300

    Added 5.2.a section algorithms

---
+++ src/chapter5/section2/a/finding/simple/formula/or/pattern/easier/BackToIntermediateMath.java
@@ -0,0 +1,80 @@
+package chapter5.section2.a.finding.simple.formula.or.pattern.easier;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 02/12/24.
+ */
+public class BackToIntermediateMath {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        int tests = FastReader.nextInt();
+
+        for (int t = 1; t <= tests; t++) {
+            double riverWidth = FastReader.nextInt();
+            int riverSpeed = FastReader.nextInt();
+            int boatSpeed = FastReader.nextInt();
+
+            double time1 = riverWidth / boatSpeed;
+            double time2 = riverWidth / Math.sqrt(boatSpeed * boatSpeed - riverSpeed * riverSpeed);
+
+            if (riverSpeed >= boatSpeed || time1 == time2) {
+                outputWriter.printLine(String.format("Case %d: can't determine", t));
+            } else {
+                double timeDifference = time2 - time1;
+                outputWriter.printLine(String.format("Case %d: %.3f", t, timeDifference));
+            }
+        }
+        outputWriter.flush();
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
