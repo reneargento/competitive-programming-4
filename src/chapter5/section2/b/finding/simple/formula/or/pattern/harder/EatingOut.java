6f056e791a56e92f7b3f47553ca91934c8f46143
Rene Argento <rene.argento@gmail.com>
2025-01-18 16:27:12 -0300

    Added 5.2.b section algorithms

---
+++ src/chapter5/section2/b/finding/simple/formula/or/pattern/harder/EatingOut.java
@@ -0,0 +1,73 @@
+package chapter5.section2.b.finding.simple.formula.or.pattern.harder;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 15/01/25.
+ */
+public class EatingOut {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        int menuItems = FastReader.nextInt();
+        int aliceItems = FastReader.nextInt();
+        int bobItems = FastReader.nextInt();
+        int claraItems = FastReader.nextInt();
+
+        if (menuItems >= (aliceItems + bobItems + claraItems) / 2.0) {
+            outputWriter.printLine("possible");
+        } else {
+            outputWriter.printLine("impossible");
+        }
+        outputWriter.flush();
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
