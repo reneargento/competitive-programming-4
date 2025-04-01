50c89895735b9c9fb0e8ac7450b00db2c936bdf2
Rene Argento <rene.argento@gmail.com>
2025-04-01 02:30:58 +0200

    Added 5.2.f section algorithms

---
+++ src/chapter5/section2/f/logarithm/exponentiation/power/SaveFromRadiation.java
@@ -0,0 +1,86 @@
+package chapter5.section2.f.logarithm.exponentiation.power;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 26/03/25.
+ */
+public class SaveFromRadiation {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        int tests = FastReader.nextInt();
+
+        for (int t = 1; t <= tests; t++) {
+            long bottles = FastReader.nextLong() + 1;
+            int minimumRats = log2Ceil(bottles);
+            outputWriter.printLine(String.format("Case %d: %d", t, minimumRats));
+        }
+        outputWriter.flush();
+    }
+
+    // It has to be computed manually to avoid floating point precision errors
+    private static int log2Ceil(long number) {
+        int log2 = 0;
+        long currentNumber = 1;
+
+        while (currentNumber < number) {
+            currentNumber *= 2;
+            log2++;
+        }
+        return log2;
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        private static long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
