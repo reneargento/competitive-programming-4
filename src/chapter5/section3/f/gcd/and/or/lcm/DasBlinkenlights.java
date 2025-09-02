3d101dc96ac6a4499ba1bafefe908fd55b86f40f
Rene Argento <rene.argento@gmail.com>
2025-09-02 02:48:46 +0200

    Added 5.3.f section algorithms

---
+++ src/chapter5/section3/f/gcd/and/or/lcm/DasBlinkenlights.java
@@ -0,0 +1,87 @@
+package chapter5.section3.f.gcd.and.or.lcm;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 14/08/25.
+ */
+public class DasBlinkenlights {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+
+        int interval1 = FastReader.nextInt();
+        int interval2 = FastReader.nextInt();
+        int maxSeconds = FastReader.nextInt();
+
+        int lcm = lcm(interval1, interval2);
+        if (lcm <= maxSeconds) {
+            outputWriter.printLine("yes");
+        } else {
+            outputWriter.printLine("no");
+        }
+        outputWriter.flush();
+    }
+
+    private static int gcd(int number1, int number2) {
+        while (number2 > 0) {
+            int temp = number2;
+            number2 = number1 % number2;
+            number1 = temp;
+        }
+        return number1;
+    }
+
+    public static int lcm(int number1, int number2) {
+        return number1 * number2 / gcd(number1, number2);
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
