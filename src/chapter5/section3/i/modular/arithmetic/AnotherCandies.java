c606644012eaae4d3b16018c8478199b2b2875f5
Rene Argento <rene.argento@gmail.com>
2025-10-09 04:06:20 +0200

    Added 5.3.i section algorithms

---
+++ src/chapter5/section3/i/modular/arithmetic/AnotherCandies.java
@@ -0,0 +1,83 @@
+package chapter5.section3.i.modular.arithmetic;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 08/10/25.
+ */
+public class AnotherCandies {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        int tests = FastReader.nextInt();
+
+        for (int t = 0; t < tests; t++) {
+            int children = FastReader.nextInt();
+            long candies = 0;
+            for (int c = 0; c < children; c++) {
+                candies += FastReader.nextLong();
+                candies %= children;
+            }
+
+            if (candies == 0) {
+                outputWriter.printLine("YES");
+            } else {
+                outputWriter.printLine("NO");
+            }
+        }
+        outputWriter.flush();
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        private static long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
