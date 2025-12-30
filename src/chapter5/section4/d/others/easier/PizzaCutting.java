514913df803ea0e36c18e83e13e38f0a22a8a6e8
Rene Argento <rene.argento@gmail.com>
2025-12-30 02:57:18 +0100

    Added 5.4.d section algorithms

---
+++ src/chapter5/section4/d/others/easier/PizzaCutting.java
@@ -0,0 +1,70 @@
+package chapter5.section4.d.others.easier;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Rene Argento on 20/12/25.
+ */
+public class PizzaCutting {
+
+    public static void main(String[] args) throws IOException {
+        FastReader.init();
+        OutputWriter outputWriter = new OutputWriter(System.out);
+        long cuts = FastReader.nextInt();
+
+        while (cuts >= 0) {
+            long slices = cuts * (cuts + 1) / 2 + 1;
+            outputWriter.printLine(slices);
+            cuts = FastReader.nextInt();
+        }
+        outputWriter.flush();
+    }
+
+    private static class FastReader {
+        private static BufferedReader reader;
+        private static StringTokenizer tokenizer;
+
+        static void init() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = new StringTokenizer("");
+        }
+
+        private static String next() throws IOException {
+            while (!tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        private static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+}
